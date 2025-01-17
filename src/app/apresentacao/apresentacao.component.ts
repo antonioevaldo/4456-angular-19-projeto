95e0f8c76bebbfed09df2d784bad49e6a69bd583
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-01-17 16:21:04 -0300

    Vídeo 1.5

--- src/app/apresentacao/apresentacao.component.ts
+++ src/app/apresentacao/apresentacao.component.ts
@@ -1,9 +1,9 @@
-import { DatePipe, TitleCasePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-apresentacao',
-  imports: [DatePipe, TitleCasePipe],
+  imports: [DatePipe, TitleCasePipe, CurrencyPipe],
   templateUrl: './apresentacao.component.html',
   styleUrl: './apresentacao.component.css'
 })
