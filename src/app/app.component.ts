b5c4f0bbc87ad1990d761563425f95906a5eb8b3
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-01-20 16:06:25 -0300

    Vídeo 2.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ApresentacaoComponent } from './apresentacao/apresentacao.component';
+import { FormNovaTransacaoComponent } from "./form-nova-transacao/form-nova-transacao.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ApresentacaoComponent],
+  imports: [ApresentacaoComponent, FormNovaTransacaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
