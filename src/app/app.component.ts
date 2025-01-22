2f38666344de49d83d7793ffc560768daf30e7fb
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-01-22 18:05:06 -0300

    Mão na massa: impedindo saque maior que saldo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,10 @@ export class AppComponent {
   });
 
   processarTransacao(transacao: Transacao) {
+    if (transacao.tipo === TipoTransacao.SAQUE && transacao.valor > this.saldo()) {
+      return alert('Saldo insuficiente!')
+    }
+
     this.transacoes.update((listaAtual) => [...listaAtual, transacao]);
   }
 }
