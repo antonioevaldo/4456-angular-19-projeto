f4833b8bbbaaae30a288cb86a16636e1a993ca99
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-01-22 15:31:32 -0300

    fix: cria componente `BoasVindas`

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,13 +1,13 @@
-import { CurrencyPipe, DatePipe, TitleCasePipe } from '@angular/common';
+import { CurrencyPipe } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { BoasVindasComponent } from "./boas-vindas/boas-vindas.component";
 
 @Component({
   selector: 'app-banner',
-  imports: [DatePipe, TitleCasePipe, CurrencyPipe],
+  imports: [CurrencyPipe, BoasVindasComponent],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
-  dataAtual = new Date();
   saldo = input.required<number>();
 }
