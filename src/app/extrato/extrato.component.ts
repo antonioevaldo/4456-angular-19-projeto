ef9930035466339ace49b16200309bde3e7fe8be
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-01-23 17:10:04 -0300

    Vídeo 5.1

---
+++ src/app/extrato/extrato.component.ts
@@ -0,0 +1,12 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-extrato',
+  imports: [CurrencyPipe],
+  templateUrl: './extrato.component.html',
+  styleUrl: './extrato.component.css'
+})
+export class ExtratoComponent {
+
+}
