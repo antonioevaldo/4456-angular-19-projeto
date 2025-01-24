b17a200704dd58ab5ce680f797c94324faa9d867
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-01-24 16:55:30 -0300

    fix: processa valor negativo no TS

--- src/app/extrato/transacao/transacao.component.ts
+++ src/app/extrato/transacao/transacao.component.ts
@@ -1,5 +1,5 @@
-import { Component, input } from '@angular/core';
-import { Transacao } from '../../modelos/transacao';
+import { Component, computed, input } from '@angular/core';
+import { TipoTransacao, Transacao } from '../../modelos/transacao';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
@@ -10,4 +10,12 @@ import { CurrencyPipe, DatePipe } from '@angular/common';
 })
 export class TransacaoComponent {
   transacao = input.required<Transacao>();
+
+  valor = computed(() => {
+    if (this.transacao().tipo === TipoTransacao.SAQUE) {
+      return -this.transacao().valor;
+    }
+
+    return this.transacao().valor;
+  });
 }
